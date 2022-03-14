5b85d941fe313e5b5e27c50bd01bfb2bf7dd1560
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-14 16:42:52 +1100

    Create add functionality in Home component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc, GeoPoint } from 'firebase/firestore';
 import { db } from './../firebase/firebase-config';
 
+// Basic Firebase CRUD functionality implemented using the following tutorial:
+// CRUD Tutorial Using React + Firebase | Firebase 9 and Firestore Tutorial
+// Reference: https://www.youtube.com/watch?v=jCY6DH8F4oc
+
 function Home() {
+  const [inputName, setInputName] = useState('');
+  const [inputLat, setInputLat] = useState(0);
+  const [inputLong, setInputLong] = useState(0);
   const [locations, setLocations] = useState([]);
   const locationsCollectionRef = collection(db, 'locations');
+
+  const createLocation = async () => {
+    await addDoc(locationsCollectionRef, {
+      name: inputName,
+      location: new GeoPoint(inputLat, inputLong)
+    });
+  };
 
   useEffect(() => {
     const getLocations = async () => {
@@ -13,10 +27,28 @@ function Home() {
     };
 
     getLocations();
-  }, []);
+  }, [locationsCollectionRef]);
 
   return (
     <div>
+      Name:
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(event) => setInputName(event.target.value)}
+      />
+      Location:
+      <input
+        type="number"
+        placeholder="latitude"
+        onChange={(event) => setInputLat(parseFloat(event.target.value))}
+      />
+      <input
+        type="number"
+        placeholder="longitude"
+        onChange={(event) => setInputLong(parseFloat(event.target.value))}
+      />
+      <button onClick={createLocation}>Add Location</button>
       <ul>
         {locations.map((location) => (
           <li key={location.id}>
