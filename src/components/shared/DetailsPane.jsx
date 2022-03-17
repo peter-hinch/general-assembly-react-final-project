7028c0570f13ec490ba700473d642ba922d840ea
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-17 16:32:07 +1100

    Add DetailsPane component.

---
+++ src/components/shared/DetailsPane.jsx
@@ -0,0 +1,11 @@
+function DetailsPane() {
+  return (
+    <div className="details-pane">
+      <h2>Venue Name</h2>
+      <p>Rating: X stars</p>
+      <p>Description</p>
+    </div>
+  );
+}
+
+export default DetailsPane;
