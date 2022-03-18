27ff24e0745f71d6833fdad6cbbe63c9e8c2c644
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-18 12:38:34 +1100

    Add paragraph element.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,9 +4,11 @@ function Footer() {
   return (
     <footer className="container">
       <div className="acknowledge-country">
-        We acknowledge the Traditional Custodians of the land on which we
-        operate - The Wurundjeri people of the Kulin Nation, and pay our
-        respects to elders past, present and emerging.
+        <p>
+          We acknowledge the Traditional Custodians of the land on which we
+          operate - The Wurundjeri people of the Kulin Nation, and pay our
+          respects to elders past, present and emerging.
+        </p>
       </div>
       <small>&copy;{year} Peter Hinch, All rights reserved.</small>
     </footer>
