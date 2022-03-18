36ce626b6b1eff6d032cd54e5055297eafaf0c0b
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-18 12:01:12 +1100

    Add acknowledgment of country.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,6 +3,11 @@ function Footer() {
 
   return (
     <footer className="container">
+      <div className="acknowledge-country">
+        We acknowledge the Traditional Custodians of the land on which we
+        operate - The Wurundjeri people of the Kulin Nation, and pay our
+        respects to elders past, present and emerging.
+      </div>
       <small>&copy;{year} Peter Hinch, All rights reserved.</small>
     </footer>
   );
