817cc7d7040b48e2788b784ae277f4f38dc157f7
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-18 14:13:16 +1100

    Add <div> elements for flag and copyright info.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,13 +4,16 @@ function Footer() {
   return (
     <footer className="container">
       <div className="acknowledge-country">
+        <div className="flag"></div>
         <p>
-          We acknowledge the Traditional Custodians of the land on which we
-          operate - The Wurundjeri people of the Kulin Nation, and pay our
-          respects to elders past, present and emerging.
+          We acknowledge the Traditional Custodians of the land on which
+          RemoteSpot is based - The Wurundjeri people of the Kulin Nation, and
+          pay our respects to elders past, present and emerging.
         </p>
       </div>
-      <small>&copy;{year} Peter Hinch, All rights reserved.</small>
+      <div className="copyright-info">
+        <small>&copy;{year} Peter Hinch, All rights reserved.</small>
+      </div>
     </footer>
   );
 }
