fc81debb49b46d1494bff48202630460595f6788
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-18 12:37:45 +1100

    Add demonstration content to verify styling.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>H1 - Header 1 - 36px Medium Roboto</h1>
+      <h2>H2 - Header 2 - 32px Medium Roboto</h2>
+      <h3>H3 - Header 3 - 28px Medium Roboto</h3>
+      <h4>H4 - Header 4 - 24px Medium Roboto</h4>
+      <h5>H5 - Header 5 - 18px Medium Roboto</h5>
+      <h6>H6 - Header 6 - 14px Medium Roboto</h6>
+      <p>Paragraph - 16px Regular Roboto</p>
+    </div>
+  );
 }
 
 export default Home;
