6d5e4e67ee1c986e8c899c69880470356c3f9dee
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-22 00:09:30 +1100

    Add conditional rendering for 'rating' property.

--- src/components/shared/DetailsPane.tsx
+++ src/components/shared/DetailsPane.tsx
@@ -4,7 +4,9 @@ function DetailsPane({ currentPlace }: { currentPlace: Feature }) {
   return (
     <div className="details-pane">
       <h2>{currentPlace.properties.name}</h2>
-      <p>Rating: {currentPlace.properties.rating} stars</p>
+      {currentPlace.properties.rating !== null && (
+        <p>Rating: {currentPlace.properties.rating} stars</p>
+      )}
       <p>{currentPlace.properties.description}</p>
       <small>{currentPlace.properties.place_id}</small>
     </div>
