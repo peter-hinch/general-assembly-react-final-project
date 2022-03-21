c5eb99b2eaeb37675e540f8dff4638c7b40d04fd
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-21 18:00:50 +1100

    Add AnimatePresence and useLocation for route-based animations.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import NavBar from './components/layout/NavBar';
 import Home from './pages/Home';
 import FirestoreEx from './pages/FirestoreEx';
@@ -7,23 +8,29 @@ import Footer from './components/layout/Footer';
 import PlacesEx from './pages/PlacesEx';
 
 function App() {
+  // Note: for useLocation to function correctly the Router must be defined in
+  // a higher level component (in this case index.js).
+  const location = useLocation();
+
   return (
-    <Router>
+    <>
       <NavBar />
       <main className="container">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/mapview" exact element={<MapView />} />
-          <Route
-            path="/places"
-            exact
-            element={<PlacesEx onChange={() => null} />}
-          />
-          <Route path="/firestore" exact element={<FirestoreEx />} />
-        </Routes>
+        <AnimatePresence exitBeforeEnter>
+          <Routes location={location} key={location.key}>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/mapview" exact element={<MapView />} />
+            <Route
+              path="/places"
+              exact
+              element={<PlacesEx onChange={() => null} />}
+            />
+            <Route path="/firestore" exact element={<FirestoreEx />} />
+          </Routes>
+        </AnimatePresence>
       </main>
       <Footer />
-    </Router>
+    </>
   );
 }
 
