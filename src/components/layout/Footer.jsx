02ff95b41f27c792ac5a386c24bee537ee7508cc
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-22 14:42:37 +1100

    Add links to Footer component.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,26 @@
+import { NavLink } from 'react-router-dom';
+
 function Footer() {
   const year = new Date().getFullYear();
 
   return (
     <footer className="container">
+      <nav>
+        <ul className="footer-links">
+          <li className="footer-link">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="footer-link">
+            <NavLink to="/map">Map</NavLink>
+          </li>
+          <li className="footer-link">
+            <NavLink to="/places">Places Example</NavLink>
+          </li>
+          <li className="footer-link">
+            <NavLink to="/firestore">Firestore Example</NavLink>
+          </li>
+        </ul>
+      </nav>
       <div className="copyright-info">
         <small>&copy;{year} Peter Hinch, All rights reserved.</small>
       </div>
