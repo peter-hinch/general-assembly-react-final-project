94c8644130e0a9a3bafe5f9fc7c0d99bdd216090
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-22 15:17:46 +1100

    Move sample text to temp. component StylingEx.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,7 @@ function Home() {
       exit="exit"
       transition={transition}
     >
-      <h1>H1 - Header 1 - 36px Medium Roboto</h1>
-      <h2>H2 - Header 2 - 32px Medium Roboto</h2>
-      <h3>H3 - Header 3 - 28px Medium Roboto</h3>
-      <h4>H4 - Header 4 - 24px Medium Roboto</h4>
-      <h5>H5 - Header 5 - 18px Medium Roboto</h5>
-      <h6>H6 - Header 6 - 14px Medium Roboto</h6>
-      <p>Paragraph - 16px Regular Roboto</p>
+      <h1>Welcome to RemoteSpot</h1>
     </motion.div>
   );
 }
