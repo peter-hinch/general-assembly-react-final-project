84cc68c878a6faa79493e6339c8ce609e0331782
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-22 16:20:37 +1100

    Create component PlaceSearch .

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import PlaceSearch from './PlaceSearch';
 
 function NavBar() {
   return (
@@ -6,10 +7,13 @@ function NavBar() {
       <div className="container">
         <nav>
           <ul className="nav-links">
-            <li className="nav-link">
+            <li className="nav-home">
               <NavLink to="/">
                 <h1>RemoteSpot</h1>
               </NavLink>
+            </li>
+            <li className="nav-search">
+              <PlaceSearch />
             </li>
             <li className="nav-link">
               <NavLink to="/map">Map</NavLink>
---
+++ src/components/layout/PlaceSearch.jsx
@@ -0,0 +1,5 @@
+function PlaceSearch({ query }) {
+  return <input type="text" placeholder="search" />;
+}
+
+export default PlaceSearch;
