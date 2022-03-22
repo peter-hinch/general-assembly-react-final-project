74ef8705fb75e139d254b979af298e5992ea095d
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-22 15:18:08 +1100

    Create temp. component StylingEx for sample styling.

---
+++ src/pages/StylingEx.jsx
@@ -0,0 +1,25 @@
+import { motion } from 'framer-motion';
+import { transition, pageVariants } from './../theme/GlobalVariants';
+
+function StylingEx() {
+  return (
+    <motion.div
+      variants={pageVariants}
+      key="home"
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      transition={transition}
+    >
+      <h1>H1 - Header 1 - 36px Medium Roboto</h1>
+      <h2>H2 - Header 2 - 32px Medium Roboto</h2>
+      <h3>H3 - Header 3 - 28px Medium Roboto</h3>
+      <h4>H4 - Header 4 - 24px Medium Roboto</h4>
+      <h5>H5 - Header 5 - 18px Medium Roboto</h5>
+      <h6>H6 - Header 6 - 14px Medium Roboto</h6>
+      <p>Paragraph - 16px Regular Roboto</p>
+    </motion.div>
+  );
+}
+
+export default StylingEx;
