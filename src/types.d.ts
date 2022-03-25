8e51ca97873804e03eef1f43aac858933ec0f444
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-25 11:11:47 +1100

    Remove GeoJSON related types and add type for Place.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,24 +1,5 @@
 // Declare the nested structure of the venueData FeatureCollection.
 
-interface FeatureCollection {
-  type: string;
-  features: Array<Feature>;
-}
-
-interface Feature {
-  geometry: FeatureGeometry;
-  properties: FeatureProperties;
-  type: string;
-}
-
-interface FeatureGeometry {
-  type: string;
-  coordinates: Array<number>;
-}
-
-interface FeatureProperties {
-  place_id: string;
-  name: string;
-  description: string;
-  rating: number | null;
+interface Place {
+  placeId: string;
 }
