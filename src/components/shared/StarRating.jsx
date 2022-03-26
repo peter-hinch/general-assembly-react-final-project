ac7c38915d2d1c7b36e697b379bf2b272ae31417
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-26 22:04:45 +1100

    Add basic logic to StarRating component.

--- src/components/shared/StarRating.jsx
+++ src/components/shared/StarRating.jsx
@@ -1,5 +1,20 @@
-function StarRating() {
-  return <div className="star-rating">⭐⭐⭐⭐⭐</div>;
+function StarRating({ rating }) {
+  const ratingToStars = () => {
+    // TODO - let's start with basic, will likely need a refactor
+    if (rating >= 80 && rating <= 100) {
+      return '⭐⭐⭐⭐⭐';
+    } else if (rating >= 60 && rating < 80) {
+      return '⭐⭐⭐⭐';
+    } else if (rating >= 40 && rating < 60) {
+      return '⭐⭐⭐';
+    } else if (rating >= 20 && rating < 40) {
+      return '⭐⭐';
+    } else {
+      return '⭐';
+    }
+  };
+
+  return <div className="star-rating">{ratingToStars()}</div>;
 }
 
 export default StarRating;
