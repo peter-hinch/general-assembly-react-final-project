999ece1780e9c33cfc8b006d3b6ff7a6da3ed878
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-26 16:49:44 +1100

    Revise types to suit new data structure.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,24 @@
-// Declare the nested structure of the venueData FeatureCollection.
+declare module '*.png';
 
 interface Place {
   placeId: string;
+  ratings: PlaceRatings;
+}
+
+interface PlaceRatings {
+  quiet: number;
+  friendly: number;
+  wifi: number;
+}
+
+interface PlacesApiResponse {
+  place_id: string;
+  geometry: any;
+  name: any;
+  website: string;
+  photos: any;
+  formatted_address: string;
+  formatted_phone_number: any;
+  opening_hours: any;
+  url: string;
 }
