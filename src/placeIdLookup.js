47629cf080d3482971734e09bf8f3ae1fb3064c6
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-26 16:50:25 +1100

    Add 'placeIdLookup.js' for API query (not yet working).

---
+++ src/placeIdLookup.js
@@ -0,0 +1,15 @@
+const placeIdLookup = (placeId, sessionToken) => {
+  const url = 'https://maps.googleapis.com/maps/api/place/details/json';
+  const key = process.env.REACT_APP_MAPS_PLACES_API_KEY;
+
+  fetch(`${url}?place_id=${placeId}&key=${key}&sessiontoken=${sessionToken}`, {
+    method: 'POST'
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((error) =>
+      console.log(`error fetching place information: ${error}`)
+    );
+};
+
+export default placeIdLookup;
