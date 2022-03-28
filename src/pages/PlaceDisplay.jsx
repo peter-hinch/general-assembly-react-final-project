cd788f2d7bb8256912ec8cfcd6a7772099d1f4cd
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-28 12:56:37 +1100

    Display spot rating from seed data.

--- src/pages/PlaceDisplay.jsx
+++ src/pages/PlaceDisplay.jsx
@@ -9,20 +9,27 @@ const PlaceDisplay = ({ currentPlace, ratingsData, placesApiData }) => {
     (rating) => rating.placeId === currentPlace.place_id
   );
 
+  const spotRating = spotRatingDetails[0].ratings.overall;
+
   return (
     <div>
-      <div className="rating-bar">
+      <div id="spot-rating" className="rating-bar">
         <div className="rating-lead">
           <h4>Spot Rating</h4>
-          <StarRating rating={100} />
+          <StarRating rating={spotRating} />
         </div>
-        <a href="#rating-breakdown" className="rating-link">
+        <a href="#rating-details" className="rating-link">
           More about this spot
         </a>
       </div>
       <DetailsPane currentPlace={currentPlace} />
-      <div id="rating-breakdown">
-        <h3>Spot details</h3>
+      <div id="rating-details" className="rating-bar">
+        <div className="rating-lead">
+          <h4>Rating Details</h4>
+        </div>
+        <a href="#" className="rating-link">
+          Rate this spot
+        </a>
       </div>
       <pre style={{ textAlign: 'left' }}>
         {JSON.stringify(spotRatingDetails, null, 2)}
