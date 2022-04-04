4aee31036dd832347fae1d3bb26b9f3fc93745a4
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-04 18:19:44 +1000

    Add numeric rating to StarRating component.

--- src/components/shared/StarRating.jsx
+++ src/components/shared/StarRating.jsx
@@ -2,15 +2,15 @@ function StarRating({ rating }) {
   const ratingToStars = () => {
     // TODO - let's start with basic, will likely need a refactor
     if (rating >= 80 && rating <= 100) {
-      return '⭐⭐⭐⭐⭐';
+      return `⭐⭐⭐⭐⭐ (${rating})`;
     } else if (rating >= 60 && rating < 80) {
-      return '⭐⭐⭐⭐';
+      return `⭐⭐⭐⭐ (${rating})`;
     } else if (rating >= 40 && rating < 60) {
-      return '⭐⭐⭐';
+      return `⭐⭐⭐ (${rating})`;
     } else if (rating >= 20 && rating < 40) {
-      return '⭐⭐';
+      return `⭐⭐ (${rating})`;
     } else {
-      return '⭐';
+      return `⭐ (${rating})`;
     }
   };
 
