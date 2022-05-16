aea4a9ea9bed7c73f9fe9ac89f496912cdc3cad9
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-16 13:39:03 +1000

    Add preliminary calls to action in home page.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import * as s from './Home.styles';
 import { transition, pageVariants } from '../../theme/GlobalVariants';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -13,14 +14,34 @@ const Home = () => {
       transition={transition}
     >
       <s.StyledHeroImage>
-        <s.StyledHeroHeaders>
+        <div>
           <h1>Welcome to RemoteSpot</h1>
           <h2>Find your new favourite remote work space.</h2>
-        </s.StyledHeroHeaders>
+        </div>
+        <s.StyledImageAttribution href="https://unsplash.com/photos/LCcFI_26diA">
+          Image: Brooke Cagle (Unsplash)
+        </s.StyledImageAttribution>
       </s.StyledHeroImage>
-      <s.StyledImageAttribution href="https://unsplash.com/photos/LCcFI_26diA">
-        Image: Brooke Cagle (Unsplash)
-      </s.StyledImageAttribution>
+      <s.StyledInstructionsSection>
+        <h3>Find a venue</h3>
+        <p>
+          Use the search bar or take a look at the <Link to="/map">Map</Link>{' '}
+          page to find venues near you.
+        </p>
+      </s.StyledInstructionsSection>
+      <s.StyledInstructionsSection>
+        <h3>Check out the venue ratings</h3>
+        <p>
+          View ratings for any venue that has been reviewed by other workers.
+        </p>
+      </s.StyledInstructionsSection>
+      <s.StyledInstructionsSection>
+        <h3>Share your own experience</h3>
+        <p>
+          Add your experience and help others find a location that they will
+          love to work from.
+        </p>
+      </s.StyledInstructionsSection>
     </motion.div>
   );
 };
--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -2,23 +2,38 @@ import styled from 'styled-components';
 import imgHero from './../../assets/brooke-cagle-LCcFI_26diA-unsplash.jpg';
 
 export const StyledHeroImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   margin: -1.5rem -1.5rem 0;
-  padding: 2.25rem 1.5rem 1.5rem;
+  padding: 1.5rem 1.5rem 0.5rem;
   aspect-ratio: 1.5 / 1;
+  color: var(--color-white);
   background: no-repeat url(${imgHero});
   background-size: 100%;
   border-radius: 1.5rem 1.5rem 0 0;
 `;
 
-export const StyledHeroHeaders = styled.div`
-  color: white;
-`;
-
 export const StyledImageAttribution = styled.a`
   font-size: 80%;
-  color: var(--color-neutral-variant-30);
+  color: var(--color-neutral-90);
+  align-self: flex-end;
 
   &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const StyledInstructionsSection = styled.section`
+  margin: 0 -1.5rem;
+  padding: 1.5rem;
+  color: var(--color-white);
+  background: var(--color-tertiary-40);
+
+  & a {
+    color: var(--color-white);
+  }
+  & a:hover {
     text-decoration: underline;
   }
 `;
